dae4d267bbb920a88ba85f9ac8b2d4ffd51a45e0
Jorge Pezzuol <jorge_pezzuol@hotmail.com>
2021-01-10 23:38:52 +0000

    Minor fixes

--- src/App.vue
+++ src/App.vue
@@ -1,60 +1,18 @@
 <template>
   <div id="app">
     <Loader />
-    <nav
-      v-if="alert.message"
-      :class="`navbar is-fixed-top notification ${alert.type} has-text-weight-bold is-size-4`"
-    >
-      {{ alert.message }}
-    </nav>
+    <Alert />
     <router-view />
   </div>
 </template>
 
 <script>
-import { mapActions, mapState } from "vuex";
 import Loader from "./components/LoaderComponent";
+import Alert from "./components/AlertComponent";
 export default {
   components: {
     Loader,
-  },
-  name: "app",
-  computed: {
-    ...mapState("alertModule", ["alert"]),
-  },
-  methods: {
-    ...mapActions("alertModule", ["clear"]),
-  },
-  watch: {
-    $route(to, from) {
-      this.clear();
-    },
+    Alert,
   },
 };
 </script>
-
-<style scoped>
-.loader-wrapper {
-  position: absolute;
-  top: 0;
-  left: 0;
-  height: 100%;
-  width: 100%;
-  background: #fff;
-  opacity: 0;
-  z-index: -1;
-  transition: opacity 0.3s;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  border-radius: 6px;
-}
-.loader {
-  height: 80px;
-  width: 80px;
-}
-.is-active {
-  opacity: 1;
-  z-index: 1;
-}
-</style>
--- src/components/AlertComponent.vue
+++ src/components/AlertComponent.vue
@@ -2,16 +2,54 @@
   <nav
     v-if="alert.message"
     :class="`navbar is-fixed-top notification ${alert.type} has-text-weight-bold is-size-4`"
-  ></nav>
+  >
+    {{ alert.message }}
+  </nav>
 </template>
 
 <script>
+import { mapActions, mapState } from "vuex";
 export default {
   name: "Alert",
   data() {
     return {};
   },
+  computed: {
+    ...mapState("alertModule", ["alert"]),
+  },
+  methods: {
+    ...mapActions("alertModule", ["clear"]),
+  },
+  watch: {
+    $route(to, from) {
+      this.clear();
+    },
+  },
 };
 </script>
 
-<style scoped></style>
+<style scoped>
+.loader-wrapper {
+  position: absolute;
+  top: 0;
+  left: 0;
+  height: 100%;
+  width: 100%;
+  background: #fff;
+  opacity: 0;
+  z-index: -1;
+  transition: opacity 0.3s;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  border-radius: 6px;
+}
+.loader {
+  height: 80px;
+  width: 80px;
+}
+.is-active {
+  opacity: 1;
+  z-index: 1;
+}
+</style>
